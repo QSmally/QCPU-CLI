31d225b6b5526906544e2eff6bd0ea7c0447e28a
QSmally <github@qbot.eu>
2021-11-15 09:19:17 +0100

    Project: initial commit

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "QCPU CLI",
+    dependencies: [ /* .package(url: /* package url */, from: "1.0.0"), */ ],
+    targets: [
+        .target(
+            name: "QCPU",
+            dependencies: []),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,4 @@
+
+# QCPU CLI
+
+> A CLI for compiling Q-code, assembling extended QCPU 2 assembly and emulating machine code.
