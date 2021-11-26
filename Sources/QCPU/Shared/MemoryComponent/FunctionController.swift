639bd316be2fc37ea574665f4f2f6f52e02f38f5
QSmally <github@qbot.eu>
2021-11-26 19:35:33 +0100

    Memory: function controller

---
+++ Sources/QCPU/Shared/MemoryComponent/FunctionController.swift
@@ -0,0 +1,37 @@
+//
+//  FunctionController.swift
+//  QCPU CLI
+//
+//  Created by Joey Smalen on 25/11/2021.
+//
+
+import Foundation
+
+struct FunctionController {
+
+    let function: String
+    let arguments: [String]
+    let statement: String
+    unowned var memoryComponent: MemoryComponent
+
+    init(_ function: String, from statement: String, memoryComponent: MemoryComponent) {
+        var components = function.components(separatedBy: .whitespaces)
+        self.function = components.removeFirst()
+        self.arguments = components
+        self.statement = statement
+        self.memoryComponent = memoryComponent
+    }
+
+    func parse() -> [String] {
+        switch function {
+            case "random":
+                return [UUID().uuidString.replacingOccurrences(of: "-", with: "_")]
+
+            case "array":
+                return []
+
+            default:
+                CLIStateController.terminate("Parse error (\(memoryComponent.name)): invalid function '\(function)'")
+        }
+    }
+}
