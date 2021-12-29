807889b349dbf70afb43a644538be236a964d44e
QSmally <github@qbot.eu>
2021-12-29 17:20:48 +0100

    Emulator: redesign emulator UI

--- Sources/QCPU/Emulator/EmulatorUI.swift
+++ Sources/QCPU/Emulator/EmulatorUI.swift
@@ -9,67 +9,62 @@ import Dispatch
 
 extension EmulatorStateController {
 
-    var kernelInformation: [String] {
-        return [String.empty, "Parameter stack"]
-            .inserted(mmu.parameters.map { " - \($0)" })
-            .inserted([String.empty, "Call stack"])
-            .inserted(mmu.addressCallStack.map { " - \($0)" })
-    }
-
-    var outputStreams: [String] {
-        return ["Ports"]
-            .inserted(outputStream.map { " - \($0)" })
-    }
-
     var columns: [[String]] {
         [
+            // Data memory
             (dataComponent?.compiled
                 .sorted { $0.key < $1.key }
-                .map { " - \(String($0).padding(toLength: 2)): \($1.value)" } ?? [])
+                .map { " - \(String($0).padding(toLength: 2)): \($1.value)".padding(toLength: 24) } ?? [])
                 .inserted("Data Memory", at: 0)
                 .inserted("(\(dataComponent?.name ?? "untitled"))", at: 1),
+
+            // Instruction memory
             (instructionComponent?.compiled
                 .sorted { $0.key < $1.key }
                 .map { " \(line == $0 ? ">" : "-") \(String($0).padding(toLength: 2)): \($1.formatted) (\($1.value))" } ?? [])
                 .inserted("Instruction Memory", at: 0)
                 .inserted("(\(instructionComponent?.name ?? "untitled"))", at: 1),
-            registers
-                .sorted { $0.key < $1.key }
-                .map { " - B\($0): \($1)" }
-                .inserted("Registers", at: 0)
-                .inserted(" - A0: \(accumulator)", at: 1)
-                .inserted(kernelInformation),
+
             [
+                // Registers
+                "Registers",
+                registers
+                    .sorted { $0.key < $1.key }
+                    .map { "B\($0.key)".padding(toLength: 4) }
+                    .inserted("A0".padding(toLength: 4), at: 0)
+                    .joined(),
+                registers
+                    .sorted { $0.key < $1.key }
+                    .map { String($0.value).padding(toLength: 4) }
+                    .inserted(String(accumulator).padding(toLength: 4), at: 0)
+                    .joined(),
+                String.empty,
+
+                // State
                 "State",
                 " - Page line: \(line)",
                 " - Segment address: \(mmu.intermediateSegmentAddress)",
-                " - Total cycles: \(cycles)"
+                " - Total cycles: \(cycles)",
+                " - Mode: \(mode)"
             ]
-                .inserted(String.empty)
-                .inserted(outputStreams)
+                // Stacks and output
+                .inserted([String.empty, "Ports"])
+                .inserted(outputStream.map { " - \($0)" })
+                .inserted([String.empty, "Parameter stack"])
+                .inserted(mmu.parameters.map { " - \($0)" })
+                .inserted([String.empty, "Call stack"])
+                .inserted(mmu.addressCallStack.map { " - \($0)" })
         ]
     }
 
-    var widthColumn: Int { 24 }
-
     func updateUI() {
+        CLIStateController.output("\u{1B}[2J")
         var columnComponent = 0
-        var nextRowIndex = 0
 
         for column in columns {
-            let rows = column
-                .map { $0.padding(toLength: widthColumn) }
-                .enumerated()
-
-            for (index, row) in rows {
-                CLIStateController.output("\u{1B}[\(index + 1);\(columnComponent * widthColumn)H")
+            for (index, row) in column.enumerated() {
+                CLIStateController.output("\u{1B}[\(index + 1);\(columnComponent * 24)H")
                 CLIStateController.output(row)
-                nextRowIndex = index + 1
-            }
-
-            for removalRowIndex in nextRowIndex...64 {
-                CLIStateController.output("\u{1B}[\(removalRowIndex + 1);\(columnComponent * widthColumn)H")
-                CLIStateController.output("".padding(toLength: widthColumn))
             }
 
             columnComponent += 1
